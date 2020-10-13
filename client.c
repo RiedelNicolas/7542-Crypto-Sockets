a7da25e45bcf24f71f84074feeb859c34ef17bd9
RiedelNicolas <nariedel99@gmail.com>
2020-10-13 17:40:23 -0300

    Subo implementacion de la biblioteca client.c

--- client.c
+++ client.c
@@ -3,3 +3,20 @@
 //
 
 #include "client.h"
+
+void clientInit(Client* this){
+    socketInit(&(this->socket));
+}
+
+int clientConnect(Client*  this, const char* host, const char* port ){
+    return socketConnect(&(this->socket),host,port);
+}
+
+ssize_t clientSend(Client* this, const char* buffer, size_t len){
+    return socketSend(&(this->socket), buffer, len);
+}
+
+
+void clientUninit(Client* this){
+    socketUninit(&(this->socket) );
+}
--- client.h
+++ client.h
@@ -4,6 +4,15 @@
 
 #ifndef CLIONPROJECTS_CLIENT_H
 #define CLIONPROJECTS_CLIENT_H
+#include "common_Socket.h"
 
+typedef struct{
+    Socket socket;
+}Client;
+
+void clientInit(Client* this);
+int clientConnect(Client*  this, const char* host, const char* port );
+ssize_t clientSend(Client* this, const char* buffer, size_t len);
+void clientUninit(Client* this);
 
 #endif //CLIONPROJECTS_CLIENT_H
