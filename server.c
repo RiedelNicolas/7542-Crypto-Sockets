a71aeb28530f16b28b65de984a77e557eafab62c
RiedelNicolas <nariedel99@gmail.com>
2020-10-13 17:42:40 -0300

    Creo header de server

---
+++ server.c
@@ -0,0 +1,5 @@
+//
+// Created by riedel on 13/10/20.
+//
+
+#include "server.h"
---
+++ server.h
@@ -0,0 +1,8 @@
+//
+// Created by riedel on 13/10/20.
+//
+
+#ifndef CLIONPROJECTS_SERVER_H
+#define CLIONPROJECTS_SERVER_H
+
+#endif //CLIONPROJECTS_SERVER_H
