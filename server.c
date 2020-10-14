5a2763026a7d4e68ea69c9813ef840b3d9a5c2a6
RiedelNicolas <nariedel99@gmail.com>
2020-10-13 21:14:21 -0300

    implementacion precaria del tda server

--- server.c
+++ server.c
@@ -3,3 +3,25 @@
 //
 
 #include "server.h"
+
+void serverInit(Server * this){
+    socketInit(&(this->socket) );
+    socketInit(&(this->peer));
+}
+
+int  serverBindAndListen(Server*  this, const char* port, int max_listen ) {
+    return socketBindAndListen(&(this->socket),port, max_listen);
+}
+
+void serverAcceptOne(Server* this){
+    socketAcceptOne(&(this->socket),&(this->peer));
+}
+
+ssize_t serverRecv(Server* this, char* buffer, size_t len){
+    return socketRecv(&(this->peer),  buffer, len);
+}
+
+void serverUninit(Server* this){
+    socketUninit(&(this->socket));
+    socketUninit(&(this->peer));
+}
--- server.h
+++ server.h
@@ -4,5 +4,17 @@
 
 #ifndef CLIONPROJECTS_SERVER_H
 #define CLIONPROJECTS_SERVER_H
+#include "common_Socket.h"
+
+typedef struct{
+    Socket socket;
+    Socket peer;
+}Server;
+
+void serverInit(Server * this);
+int serverBindAndListen(Server*  this, const char* port, int max_listen );
+void serverAcceptOne(Server* this);
+ssize_t serverRecv(Server* this, char* buffer, size_t len);
+void serverUninit(Server* this);
 
 #endif //CLIONPROJECTS_SERVER_H
