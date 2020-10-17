3b77fc893e8b397e459ac5e57a6e04cc969060c9
RiedelNicolas <nariedel99@gmail.com>
2020-10-17 16:17:36 -0300

    Implementando el encriptador.

--- common_Encryptor.c
+++ common_Encryptor.c
@@ -3,5 +3,61 @@
 //
 
 #include "common_Encryptor.h"
+#define SUCCESS 0
+#define FAILURE -1
+
+//Privadas
+
+void _encrpytorEncryptCesar(Encryptor* this,char* msg, size_t size);
+void _encryptorEncryptRC4(Encryptor* this,char* msg, size_t size);
+void _encryptorEncryptVignere(Encryptor* this,char* msg, size_t size);
+
+void _encrpytorDecryptCesar(Encryptor* this,char* msg, size_t size);
+void _encryptorDecryptRC4(Encryptor* this,char* msg, size_t size);
+void _encryptorDecryptVignere(Encryptor* this,char* msg, size_t size);
 
 
+int encryptorInit(Encryptor* this, char* method, char* key ){
+    this->key = key;
+    this->cursor = 0;
+
+    if( !strcmp(this->method, CESAR) ){
+        this->encryptFunction  =
+        this->decryptFunction  =
+        return SUCCESS ;
+    }
+    if( !strcmp(this->method, RC4) ){
+        this->encryptFunction =
+        this->decryptFunction  =
+        return SUCCESS;
+    }
+    if( !strcmp(this->method, VIGNERE) ){
+        this->encryptFunction =
+        this->decryptFunction  =
+        return SUCCESS;
+    }
+    fprintf(stderr, "unknow encryption method \n");
+    return FAILURE;
+}
+
+void encryptorEncrypt(Encryptor* this,char* msg, size_t size){
+
+}
+
+void encryptadorDecrpyt(Encryptor* this,char* msg, size_t size);
+
+void encryptorReset(Encryptor* this){
+    this->cursor = 0;
+}
+void encrpytorUninit(Encryptor* this){
+    this->key = NULL;
+    this->cursor = 0;
+}
+
+
+
+void _encrpytorCesar(Encryptor* this,char* msg, size_t size){
+
+}
+void _encryptorRC4(Encryptor* this,char* msg, size_t size);
+void _encryptorVignere(Encryptor* this,char* msg, size_t size);
--- common_Encryptor.h
+++ common_Encryptor.h
@@ -5,10 +5,27 @@
 #ifndef CLIONPROJECTS_COMMON_ENCRYPTOR_H
 #define CLIONPROJECTS_COMMON_ENCRYPTOR_H
 
+#include <stdio.h>
+#include <string.h>
+
+#define CESAR "cesar"
+#define RC4 "rc4"
+#define VIGNERE "vignere"
 
 typedef struct{
-    int fd;
-}encryptor;
+    size_t cursor; //para acordarme en que numero de iteracion estoy.
+    char* key;
+    char* method;
+    void (*encryptFunction)(struct Encryptor*, char*, char*);
+    void (*decryptFunction)(struct Encryptor*, char*, char*);
+}Encryptor;
+
+
+void encryptorInit(Encryptor* this, char* method, char* key );
+void encryptorEncrypt(Encryptor* this,char* msg, size_t size);
+void encryptadorDecrpyt(Encryptor* this,char* msg, size_t size);
+void encryptorReset(Encryptor* this);
+void encrpytorUninit(Encryptor* this);
 
 
 #endif //CLIONPROJECTS_COMMON_ENCRYPTOR_H
