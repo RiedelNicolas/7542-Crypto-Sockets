c7d40c63c9586a0ca083278ae2d7fec78cc96735
RiedelNicolas <nariedel99@gmail.com>
2020-10-20 02:08:22 -0300

    Subo a sercom para ver si pasa las pruebas.

--- client_main.c
+++ client_main.c
@@ -2,16 +2,39 @@
 // Created by riedel on 13/10/20.
 //
 
+#include <getopt.h>
+
 
 #include "client_Courier.h"
+#define POS_HOST 1
+#define POS_PORT 2
 #define SIZE_BUFFER 64
 
 int main(int argc, char** argv){
+    int opt = 0;
+    int long_index = 0;
+    char* method,* key, *host, *port;
+    host = argv[POS_HOST];
+    port  = argv[POS_PORT];
+
+    static struct option long_options[] = {
+        {"method", required_argument, NULL, 'm'},
+        {"key",    required_argument, NULL, 'k'},
+        {NULL, 0,                     NULL, 0}
+    };
+    while ((opt = getopt_long(argc, argv,":m:k:",
+                              long_options, &long_index )) != -1) {
+        switch (opt) {
+            case 'm' : method = optarg;
+                break;
+            case 'k' : key = optarg;
+                break;
+        }
+    }
+
     char buffer[SIZE_BUFFER];
     Courier courier;
-    char host[80];
-    strcpy(host,"localhost");
-    courierInit(&courier,argv[2],argv[3],host,argv[1]);
+    courierInit(&courier,method,key, host,port);
     courierRun(&courier,buffer,SIZE_BUFFER);
     courierUninit(&courier);
 }
--- server_main.c
+++ server_main.c
@@ -5,13 +5,36 @@
 #include  "server_receiver.h"
 
 #include <stdio.h>
+#include <getopt.h>
+#define POS_PORT 1
+
 #define SIZE_BUFFER 64
 
 int main(int argc, char** argv){
+    int opt = 0;
+    int long_index = 0;
+    char* method,* key, *port;
+    port  = argv[POS_PORT];
+
+    static struct option long_options[] = {
+            {"method", required_argument, NULL, 'm'},
+            {"key",    required_argument, NULL, 'k'},
+            {NULL, 0,                     NULL, 0}
+    };
+    while ((opt = getopt_long(argc, argv,":m:k:",
+                              long_options, &long_index )) != -1) {
+        switch (opt) {
+            case 'm' : method = optarg;
+                break;
+            case 'k' : key = optarg;
+                break;
+        }
+    }
+
     char buffer[SIZE_BUFFER];
     Receiver receiver;
 
-    receiverInit(&receiver,argv[2],argv[3],argv[1]);
+    receiverInit(&receiver,method,key,port);
     receiverRun(&receiver,buffer,SIZE_BUFFER);
     receiverUninit(&receiver);
 
